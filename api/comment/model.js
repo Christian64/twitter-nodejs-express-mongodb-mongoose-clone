4e73f4003c961397be0828d6d94c0c48f1c623fc
unknown <christianabreuh@gmail.com>
2021-03-13 19:06:10 -0400

    Comments just have a tweet

--- api/comment/model.js
+++ api/comment/model.js
@@ -3,7 +3,7 @@ import { Schema, model } from "mongoose";
 const commentSchema = new Schema({
     body: { type: String, required: true },
     likes: [{ type: Schema.Types.ObjectId, ref: "Like" }],
-    tweets: [{ type: Schema.Types.ObjectId, ref: "Tweet" }],
+    tweet: { type: Schema.Types.ObjectId, ref: "Tweet" },
     shares: [{ type: Schema.Types.ObjectId, ref: "Rt" }],
     author: { type: Schema.Types.ObjectId, ref: "User" },
 });
