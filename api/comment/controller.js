75c9f150dfb22afa1091d03ef9d468aa57a2c286
unknown <christianabreuh@gmail.com>
2021-03-15 20:58:47 -0400

    Comments features completed!

--- api/comment/controller.js
+++ api/comment/controller.js
@@ -1,5 +1,7 @@
 import Comment from "./model";
 import Tweet from "../tweet/model";
+import Rt from "../rt/model";
+import Like from "../like/model";
 
 export const show = async () => {
     try {
@@ -20,10 +22,10 @@ export const create = async (req, res) => {
         const comment = new Comment(req.body);
         await comment.save();
 
-        console.log(req.params)
+        console.log(req.params);
 
         const tweet = await Tweet.findById(req.params.tweetId);
-        console.log(tweet)
+        console.log(tweet);
         tweet.comments.push(comment._id);
         await tweet.save();
 
@@ -41,6 +43,60 @@ export const remove = async (req, res) => {
         tweet.comments.pull(id);
         await tweet.save();
         res.status(200).json(tweet);
+    } catch (err) {
+        console.log("This is the error:", err);
+    }
+};
+
+export const createRt = async (req, res) => {
+    const { id } = req.params;
+    req.body.author = req.user._id;
+    req.body.comment = id;
+    const rt = new Rt(req.body);
+    await rt.save();
+    const comment = await Comment.findById(id).populate("shares");
+    comment.shares.push(rt._id);
+    comment.save();
+    res.status(200).json(comment);
+};
+
+export const removeRt = async (req, res) => {
+    try {
+        const { id, rtId } = req.params;
+        await Rt.findByIdAndDelete(rtId);
+        const comment = await Comment.findById(id);
+        comment.shares.pull(id);
+        await comment.save();
+        res.status(200).json(comment);
+    } catch (err) {
+        console.log("This is the Error:", err);
+    }
+};
+
+export const createLike = async (req, res) => {
+    const { id } = req.params;
+    req.body.comment = id;
+    req.body.author = req.user._id;
+    try {
+        const like = new Like(req.body);
+        await like.save();
+        const comment = await Comment.findById(id);
+        comment.likes.push(like);
+        await comment.save();
+        res.send(comment);
+    } catch (err) {
+        console.log("This is the error:", err);
+    }
+};
+
+export const removeLike = async (req, res) => {
+    const { id, likeId } = req.params;
+    try {
+        await Like.findByIdAndDelete(likeId);
+        const comment = await Comment.findById(id).populate("likes");
+        comment.likes.pull(likeId);
+        await comment.save();
+        res.status(200).json(comment);
     } catch (err) {
         console.log("This is the error:", err);
     }
--- api/comment/route.js
+++ api/comment/route.js
@@ -1,10 +1,20 @@
 import { Router } from "express";
-import { create, remove, show } from "./controller";
+import {
+    create,
+    remove,
+    show,
+    createRt,
+    removeRt,
+    createLike,
+    removeLike,
+} from "./controller";
 const routes = Router();
 
 routes.post("/:tweetId/comment/create", create);
-routes.route("/:tweetId/comment/:id")
-    .get(show)
-    .delete(remove)
+routes.route("/:tweetId/comment/:id").get(show).delete(remove);
+routes.post("/:tweetId/comment/:id/rt", createRt);
+routes.delete("/:tweetId/comment/:id/rt/:rtId", removeRt);
+routes.post("/:tweetId/comment/:id/like", createLike);
+routes.delete("/:tweetId/comment/:id/like/:likeId", removeLike);
 
 export default routes;
